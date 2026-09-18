023a93dcf659f3d0d764525d357acc4c5b929723
tuthanika <tuthanika@gmail.com>
2026-09-18 23:56:00 +0700

    11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.custom.simswitcher"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
