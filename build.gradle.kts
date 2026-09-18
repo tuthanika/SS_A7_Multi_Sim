0cb4c57223f973c64af125bdf716732c70154f55
tuthanika <tuthanika@gmail.com>
2026-09-18 22:06:02 +0700

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(layout.buildDirectory)
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
