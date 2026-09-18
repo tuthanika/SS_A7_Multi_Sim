a5a512e80532c3844bf6385aab3e723fbdc03c49
tuthanika <tuthanika@gmail.com>
2026-09-18 22:15:47 +0700

    Update MainActivity.kt

--- app/src/main/java/com/custom/simswitcher/MainActivity.kt
+++ app/src/main/java/com/custom/simswitcher/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate()
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
